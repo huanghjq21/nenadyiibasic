19a1a19def687a46a70e746015cc34cc2a9cbea3
Huang Junqi <huanghjq21@gmail.com>
2016-05-22 21:42:35 +0800

    add FontAwesomeAsset

---
+++ _protected/assets/FontAwesomeAsset.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\assets;
+
+use yii\web\AssetBundle;
+use Yii;
+
+class FontAwesomeAsset extends AssetBundle
+{
+	public $basePath = '@webroot';
+	public $baseUrl = '@themes';
+	
+	public $css = [			
+		'font-awesome/css/font-awesome.css',
+		'font-awesome/css/font-awesome.min.css',
+	];
+	
+	public $js = [		
+	];
+	
+	public $depends = [		
+	];
+}
